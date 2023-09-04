f868e2e1a1f71101f8da8b48698b9ff1718cdac7
Riaz <mriaztda578@gmail.com>
2023-09-04 16:56:01 +0500

    day2

---
+++ Day 2/index.js
@@ -0,0 +1,24 @@
+// comparisonOperators
+// Grater then 
+var num1 = 12;
+var num2 = 24;
+// less then
+var result = num2 < num1;
+// console.log(result);
+// grater then
+var result = num2 > num1;
+// console.log(result);
+// equal to 
+var result = num2 == num1;
+// console.log(result);
+// value check and datatype check.
+var result = num1 === num2;
+// console.log(result);
+// not equal to 
+var result = num1 != num2;
+// console.log(result);
+// grater then or equal to 
+var result = num1 >= num2;
+console.log(result);
+var result = num1 <= num2;
+console.log(result);
