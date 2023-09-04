26a854768ab8394623d7c4b49da44c9cbedf8f46
Riaz <mriaztda578@gmail.com>
2023-09-04 17:21:01 +0500

    day2

--- Day 2/index.js
+++ Day 2/index.js
@@ -1,24 +1,28 @@
-// comparisonOperators
-// Grater then 
-var num1 = 12;
-var num2 = 24;
-// less then
-var result = num2 < num1;
-// console.log(result);
-// grater then
-var result = num2 > num1;
-// console.log(result);
-// equal to 
-var result = num2 == num1;
-// console.log(result);
-// value check and datatype check.
-var result = num1 === num2;
-// console.log(result);
-// not equal to 
-var result = num1 != num2;
-// console.log(result);
-// grater then or equal to 
-var result = num1 >= num2;
-console.log(result);
-var result = num1 <= num2;
-console.log(result);
+// // comparisonOperators
+// // Grater then 
+// var num1=12;
+// var num2=24;
+// // less then
+// var result: boolean = num2 < num1;
+// // console.log(result);
+// // grater then
+// var result: boolean = num2 > num1;
+// // console.log(result);
+// // equal to 
+// var result: boolean = num2 == num1;
+// // console.log(result);
+// // value check and datatype check.
+// var result: boolean = num1 === num2;
+// // console.log(result);
+// // not equal to 
+// var result: boolean = num1 != num2;
+// // console.log(result);
+// // grater then or equal to 
+// var result: boolean = num1 >= num2;
+// // console.log(result);
+// var result: boolean = num1 <= num2;
+// // console.log(result);
+// Strings
+var username = "Muhammad riaz ";
+var password = "Muhammad@1232";
+console.log("username : ", username, "Password : ", password);
--- Day 2/index.ts
+++ Day 2/index.ts
@@ -1,24 +1,41 @@
-// comparisonOperators
-// Grater then 
-var num1=12;
-var num2=24;
-// less then
-var result: boolean = num2 < num1;
-// console.log(result);
-// grater then
-var result: boolean = num2 > num1;
-// console.log(result);
-// equal to 
-var result: boolean = num2 == num1;
-// console.log(result);
-// value check and datatype check.
-var result: boolean = num1 === num2;
-// console.log(result);
-// not equal to 
-var result: boolean = num1 != num2;
-// console.log(result);
-// grater then or equal to 
-var result: boolean = num1 >= num2;
-// console.log(result);
-var result: boolean = num1 <= num2;
-// console.log(result);
+// // comparisonOperators
+// // Grater then 
+// var num1=12;
+// var num2=24;
+// // less then
+// var result: boolean = num2 < num1;
+// // console.log(result);
+// // grater then
+// var result: boolean = num2 > num1;
+// // console.log(result);
+// // equal to 
+// var result: boolean = num2 == num1;
+// // console.log(result);
+// // value check and datatype check.
+// var result: boolean = num1 === num2;
+// // console.log(result);
+// // not equal to 
+// var result: boolean = num1 != num2;
+// // console.log(result);
+// // grater then or equal to 
+// var result: boolean = num1 >= num2;
+// // console.log(result);
+// var result: boolean = num1 <= num2;
+// // console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Strings
+let username:string="Muhammad riaz "
+let password:string="Muhammad@1232"
+console.log("username : " , username , "Password : " , password);
