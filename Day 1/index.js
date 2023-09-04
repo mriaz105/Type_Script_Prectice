63274bce7bb6958f870c8125c2f55e533fe5b17b
Riaz <mriaztda578@gmail.com>
2023-09-04 16:02:26 +0500

    added more

--- Day 1/index.js
+++ Day 1/index.js
@@ -10,4 +10,23 @@ var obtainedmarks = 345;
 var admission_status = "Done";
 var subject;
 subject = "urdu";
-console.log(subject);
+// console.log(subject);
+// Operators 
+// Arithmatic operators
+// + is use to add two variables 
+var num1 = 24;
+var num2 = 12;
+var sum = num1 + num2;
+// console.log("Sum of " , num1 , "and " , num2 , "is " , sum);
+// - is used to substrect two numbers
+var difference = num1 - num2;
+// console.log("Difference  of " , num1 , "and " , num2 , "is " , difference);
+// * is used to multiply two numbers
+var product = num1 * num2;
+// console.log("product  of " , num1 , "and " , num2 , "is " , product);
+//              / is used to divide two numbers
+var divide = num1 / num2;
+// console.log("division of " , num1 , "and " , num2 , "is " , divide);
+// %is used to find reminder of two numbers
+var reminder = num1 % num2;
+console.log("reminder  of ", num1, "and ", num2, "is ", reminder);
--- Day 1/index.ts
+++ Day 1/index.ts
@@ -10,4 +10,29 @@ var obtainedmarks:number=345;
 var admission_status:String="Done";
 var subject:string;
 subject="urdu";
-console.log(subject);
+// console.log(subject);
+// Operators 
+// Arithmatic operators
+// + is use to add two variables 
+let num1:number=24;
+let num2:number=12;
+let sum:number=num1+num2;
+// console.log("Sum of " , num1 , "and " , num2 , "is " , sum);
+// - is used to substrect two numbers
+let difference:number=num1-num2;
+// console.log("Difference  of " , num1 , "and " , num2 , "is " , difference);
+// * is used to multiply two numbers
+let product:number=num1*num2;
+// console.log("product  of " , num1 , "and " , num2 , "is " , product);
+//              / is used to divide two numbers
+let divide:number=num1/num2;
+// console.log("division of " , num1 , "and " , num2 , "is " , divide);
+// %is used to find reminder of two numbers
+let reminder:number=num1%num2;
+// console.log("reminder  of " , num1 , "and " , num2 , "is " , reminder);
+// = is used to assign a value to a variable
+let Marks:number=23;
+let UserName:string="Muhammad Riaz";
+
+
+
