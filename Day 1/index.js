72f7a49e11b845989b9221511dcd2391a0691201
Riaz <mriaztda578@gmail.com>
2023-09-04 15:42:03 +0500

    day 1 added

---
+++ Day 1/index.js
@@ -0,0 +1,13 @@
+// This is prectice file of variables
+// Some variables
+var username = "Muhammad riaz";
+var marks = 23;
+var condetion = true;
+var age = 23;
+// Variables with Data type
+var agegroup = "young";
+var obtainedmarks = 345;
+var admission_status = "Done";
+var subject;
+subject = "urdu";
+console.log(subject);
---
+++ Day 1/index.ts
@@ -0,0 +1,13 @@
+// This is prectice file of variables
+// Some variables
+var username = "Muhammad riaz";
+let marks=23;
+let condetion =true;
+const age = 23;
+// Variables with Data type
+let agegroup:String="young";
+var obtainedmarks:number=345;
+var admission_status:String="Done";
+var subject:string;
+subject="urdu";
+console.log(subject);
